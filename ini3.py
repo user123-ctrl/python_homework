ecd9d99371181f182aafd7d450a8b17089f70ba5
User David <185979906+user123-ctrl@users.noreply.github.com>
2024-12-03 09:50:50 +0100

    reorganized

--- ini3.py
+++
@@ -1,8 +0,0 @@
-
-s = ('darUn2GcvX31RmSkn8WyTRegulus9QLXRWDhwaMX0CBU8EmX1PKYtTvU1Lzb96bl1lzcJ6gHxp1ibPrbtxvnFQ5682TBaNntt78BOavocettarpTYQgOOQDnyOtGuh1fwVwTBMhCOinewVeQX47alzfMGPKml0ONVT2pwJuP.')
-a = 21
-b = 27
-c = 101
-d = 108
-
-print (s[a:b+1], s[c:d+1])
